140d11b6167035299b80073e6658c11e832eb7b7
Ronaldo Andre <ronaldovh99@gmail.com>
2022-08-17 19:30:44 -0500

    Initial Commit

---
+++ reproductor_de_musica_ios/reproductor_de_musica_iosApp.swift
@@ -0,0 +1,20 @@
+//
+//  reproductor_de_musica_iosApp.swift
+//  reproductor_de_musica_ios
+//
+//  Created by Ronaldo Andre Vargas Huaman on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct reproductor_de_musica_iosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
